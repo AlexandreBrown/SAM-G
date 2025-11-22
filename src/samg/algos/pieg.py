ef3f8cc53f6c30cc81ba25f9675dcd093542ee3c
AlexandreBrown <alexandrebrown97@gmail.com>
2025-11-21 23:42:21 -0500

    Fixed logging typo

--- src/samg/algos/pieg.py
+++ src/samg/algos/pieg.py
@@ -389,6 +389,6 @@ class PIEGAgent(Agent):
         if is_time_to_evaluate:
             metrics['actor_loss'] = actor_loss.detach()
             metrics['actor_logprob'] = log_prob.detach().mean()
-            metrics['actor_ent'] = dist.detach().entropy().sum(dim=-1).mean()
+            metrics['actor_ent'] = dist.entropy().detach().sum(dim=-1).mean()
 
         return metrics
